83a7d3551ced24e1288ec105ce9fde0ad369dff3
ahmed2tech28 <120560296+ahmed2tech28@users.noreply.github.com>
2024-03-01 23:40:13 +0500

    Adding full pakage

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center dark:bg-black dark:text-white">
+      <h1>Start By Editing src/App.jsx</h1>
+    </div>
+  );
+};
+
+export default App;
